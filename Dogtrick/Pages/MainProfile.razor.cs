bae2b76913f2c77bf5392a14643c46b29336de3e
GabrielDeBruycker <gabriel.bruycker@tenstarsimulation.com>
2023-04-21 11:42:43 +0200

    Added so all dogs under Member are listed
    Added so all dogs under Member are listed

--- Dogtrick/Pages/MainProfile.razor.cs
+++ Dogtrick/Pages/MainProfile.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Entities;
 using Infrastructure.Repository;
+using Infrastructure.Service.ViewModels;
 using Microsoft.AspNetCore.Components;
 
 namespace Dogtrick.Pages
@@ -9,6 +10,8 @@ namespace Dogtrick.Pages
         [Inject]
         public IUserRepository _userRepository { get; set; }
         [Inject]
+        public IDogRepository _dogRepository { get; set; }
+        [Inject]
         public NavigationManager _navigationManager { get; set; }
 
         [Parameter]
@@ -16,12 +19,14 @@ namespace Dogtrick.Pages
         private Guid ParsedMemberId { get; set; }
 
         public User User { get; set; }
+        public List<DogViewModel> Dogs { get; set; }
 
 
         protected override async Task OnInitializedAsync()
         {
             ParsedMemberId = Guid.Parse(MemberId);
             User = await _userRepository.GetMemberOnId(ParsedMemberId);
+            Dogs = await _dogRepository.GetMemberDogsOnId(ParsedMemberId);
         }
 
         public void EditProfile()
